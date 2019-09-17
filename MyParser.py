805115f2d76c5a008376797c47b61b4ee6448dd9
llouislu <louis.dev@outlook.com>
2019-09-17 12:06:38 +1200

    refactor switch statements: step 1

--- MyParser.py
+++ MyParser.py
@@ -1,9 +1,28 @@
+from functools import partial
 from TIGr import AbstractParser
 
 
 class MyParser(AbstractParser):
     def __init__(self, drawer):
         super().__init__(drawer)
+        self.no_parameter_commands = {
+            'D': self.drawer.pen_down,
+            'U': self.drawer.pen_up
+        }
+        self.one_parameter_commands = {
+            'P': self.drawer.select_pen,
+            'X': self.drawer.go_along,
+            'Y': self.drawer.go_down,
+            'C': self.drawer.draw_circle,
+            'R': self.drawer.draw_rectangle,
+            'T': self.drawer.draw_triangle
+        }
+        self.draw_line_commands = {
+            'N': partial(self.drawer.draw_line, 90 * 0),
+            'E': partial(self.drawer.draw_line, 90 * 1),
+            'S': partial(self.drawer.draw_line, 90 * 2),
+            'W': partial(self.drawer.draw_line, 90 * 3)
+        }
 
     def parse(self, raw_source):
         self.source = raw_source
@@ -15,34 +34,12 @@ class MyParser(AbstractParser):
                 self.data = int(tempArr[1])
             except:
                 self.data = 0
-            if self.command == 'D':
-                self.drawer.pen_down()
-            if self.command == 'P':
-                self.drawer.select_pen(self.data)
-            if self.command == 'N':
-                self.drawer.draw_line(0, self.data)
-            if self.command == 'E':
-                self.drawer.draw_line(90, self.data)
-            if self.command == 'S':
-                self.drawer.draw_line(180, self.data)
-            if self.command == 'W':
-                self.drawer.draw_line(270, self.data)
-            if self.command == 'D':
-                self.drawer.pen_down()
-            if self.command == 'P':
-                self.drawer.select_pen(self.data)
-            if self.command == 'X':
-                self.drawer.go_along(self.data)
-            if self.command == 'Y':
-                self.drawer.go_down(self.data)
-            if self.command == 'U':
-                self.drawer.pen_up()
-            if self.command == 'C':
-                self.drawer.draw_circle(self.data)
-            if self.command == 'R':
-                self.drawer.draw_rectangle(self.data)
-            if self.command == 'T':
-                self.drawer.draw_triangle(self.data)
+            if self.command in self.no_parameter_commands:
+                self.no_parameter_commands[self.command]()
+            elif self.command in self.one_parameter_commands:
+                self.one_parameter_commands[self.command](self.data)
+            elif self.command in self.draw_line_commands:
+                self.draw_line_commands[self.command](self.data)
         try:
             self.drawer.end()
         except:
